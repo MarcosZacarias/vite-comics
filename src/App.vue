dd239ca58215a4b9547abd48d52b36f6c271eba3
MarcosZacarias <marcoszacariasdaniel@gmail.com>
2023-09-08 15:12:04 +0200

    add: files basic form

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  data() {
+    return {
+      message: "hello",
+    };
+  },
+};
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+</template>
+
+<style lang="scss">
+h1 {
+  color: red;
+}
+</style>
